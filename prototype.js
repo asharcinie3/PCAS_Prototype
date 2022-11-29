a07d770ba86d0fad32cd9de23bd5c6ce719dbca8
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-29 17:59:09 -0500

    add java to show div

---
+++ prototype.js
@@ -0,0 +1,8 @@
+function button11() {
+    var x = document.getElementById("e1-1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
