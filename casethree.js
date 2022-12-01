d8b5b9d753736cc4161b32a2419ac6329e05131c
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 20:54:01 -0500

    update case three page

---
+++ casethree.js
@@ -0,0 +1,11 @@
+document.getElementById("b8").onclick = () =>  {
+    document.getElementById('e8').style.display = "block";
+}
+
+document.getElementById("b9").onclick = () =>  {
+    document.getElementById('e9').style.display = "block";
+}
+
+document.getElementById("b10").onclick = () =>  {
+    document.getElementById('e10').style.display = "block";
+}
