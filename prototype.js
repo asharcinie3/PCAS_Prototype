a2d1cac809fa2f02d030cb4f7240db7724e9ef6d
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 19:56:21 -0500

    add same functionality for other buttons

--- prototype.js
+++ prototype.js
@@ -1,3 +1,16 @@
 document.getElementById("b1").onclick = () =>  {
     document.getElementById('e1-1').style.display = "block";
 }
+
+document.getElementById("b1-2").onclick = () =>  {
+    document.getElementById('e1-2').style.display = "block";
+}
+
+document.getElementById("b1-3").onclick = () =>  {
+    document.getElementById('e1-3').style.display = "block";
+}
+
+document.getElementById("b1-4").onclick = () =>  {
+    document.getElementById('e1-4').style.display = "block";
+}
+
