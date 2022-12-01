327bb20c809cfc820968b257884fa338959bf150
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 20:50:18 -0500

    update 2

--- casetwo.js
+++ casetwo.js
@@ -1,11 +1,17 @@
 document.getElementById("b5").onclick = () =>  {
     document.getElementById('e5').style.display = "block";
+    document.getElementById('e6').style.display = "none";
+    document.getElementById('e7').style.display = "none";
 }
 
 document.getElementById("b6").onclick = () =>  {
     document.getElementById('e6').style.display = "block";
+    document.getElementById('e5').style.display = "none";
+    document.getElementById('e7').style.display = "none";
 }
 
 document.getElementById("b7").onclick = () =>  {
     document.getElementById('e7').style.display = "block";
+    document.getElementById('e5').style.display = "none";
+    document.getElementById('e6').style.display = "none";
 }
