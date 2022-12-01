a0071a9d6f81dbe6e6e493e1335c7036512f5a96
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 20:48:23 -0500

    new test for case 2 functionality

---
+++ caseone.js
@@ -0,0 +1,28 @@
+document.getElementById("b1").onclick = () =>  {
+    document.getElementById('e1').style.display = "block";
+    document.getElementById('e2').style.display = "none";
+    document.getElementById('e3').style.display = "none";
+    document.getElementById('e4').style.display = "none";
+}
+
+document.getElementById("b2").onclick = () =>  {
+    document.getElementById('e2').style.display = "block";
+    document.getElementById('e1').style.display = "none";
+    document.getElementById('e3').style.display = "none";
+    document.getElementById('e4').style.display = "none";
+}
+
+document.getElementById("b3").onclick = () =>  {
+    document.getElementById('e3').style.display = "block";
+    document.getElementById('e1').style.display = "none";
+    document.getElementById('e2').style.display = "none";
+    document.getElementById('e4').style.display = "none";
+}
+
+document.getElementById("b4").onclick = () =>  {
+    document.getElementById('e4').style.display = "block";
+    document.getElementById('e1').style.display = "none";
+    document.getElementById('e2').style.display = "none";
+    document.getElementById('e3').style.display = "none";
+}
+
