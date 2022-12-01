21e4b3728c7a12410ce63b032160429227acd6ec
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 19:58:21 -0500

    attempt 2

--- prototype.js
+++ prototype.js
@@ -1,16 +1,16 @@
 document.getElementById("b1").onclick = () =>  {
-    document.getElementById('e1-1').style.display = "block";
+    document.getElementById('e1').style.display = "block";
 }
 
-document.getElementById("b1-2").onclick = () =>  {
-    document.getElementById('e1-2').style.display = "block";
+document.getElementById("b2").onclick = () =>  {
+    document.getElementById('e2').style.display = "block";
 }
 
-document.getElementById("b1-3").onclick = () =>  {
-    document.getElementById('e1-3').style.display = "block";
+document.getElementById("b3").onclick = () =>  {
+    document.getElementById('e3').style.display = "block";
 }
 
-document.getElementById("b1-4").onclick = () =>  {
-    document.getElementById('e1-4').style.display = "block";
+document.getElementById("b4").onclick = () =>  {
+    document.getElementById('e4').style.display = "block";
 }
 
