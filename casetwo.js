4c51d9e179fdf230dada8ab2f0644d438b4b106e
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-01 15:57:51 -0500

    again

--- casetwo.js
+++ casetwo.js
@@ -1,10 +1,10 @@
-document.getElementById("b5").onclick = () =>  {
+/*document.getElementById("b5").onclick = () =>  {
     document.getElementById('e5').style.display = "block";
     document.getElementById('e6').style.display = "none";
     document.getElementById('e7').style.display = "none";
 }
 
-/*document.getElementById("b6").onclick = () =>  {
+document.getElementById("b6").onclick = () =>  {
     document.getElementById('e6').style.display = "block";
     document.getElementById('e5').style.display = "none";
     document.getElementById('e7').style.display = "none";
