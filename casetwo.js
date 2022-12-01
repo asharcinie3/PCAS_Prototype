0fb18382b4dc46d2f5bbcb447a3f50bd3ea5c9f4
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 20:49:15 -0500

    it worked! update it now

--- casetwo.js
+++ casetwo.js
@@ -1,3 +1,11 @@
 document.getElementById("b5").onclick = () =>  {
     document.getElementById('e5').style.display = "block";
 }
+
+document.getElementById("b6").onclick = () =>  {
+    document.getElementById('e6').style.display = "block";
+}
+
+document.getElementById("b7").onclick = () =>  {
+    document.getElementById('e7').style.display = "block";
+}
