e654a9052272c2c03d69fddddc76acfd364373ec
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 20:55:28 -0500

    update case 3 page

--- casethree.js
+++ casethree.js
@@ -1,11 +1,17 @@
 document.getElementById("b8").onclick = () =>  {
     document.getElementById('e8').style.display = "block";
+    document.getElementById('e9').style.display = "none";
+    document.getElementById('e10').style.display = "none";
 }
 
 document.getElementById("b9").onclick = () =>  {
     document.getElementById('e9').style.display = "block";
+    document.getElementById('e8').style.display = "none";
+    document.getElementById('e10').style.display = "none";
 }
 
 document.getElementById("b10").onclick = () =>  {
     document.getElementById('e10').style.display = "block";
+    document.getElementById('e8').style.display = "none";
+    document.getElementById('e9').style.display = "none";
 }
