ab40725369d25dd767c8f04780ed2dfbf4fac67a
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-30 20:35:07 -0500

    again

--- prototype.js
+++ prototype.js
@@ -30,19 +30,13 @@ document.getElementById("b4").onclick = () =>  {
 /* cover functionality for case two page */
 document.getElementById("b5").onclick = () =>  {
     document.getElementById('e5').style.display = "block";
-    document.getElementById('e6').style.display = "none";
-    document.getElementById('e7').style.display = "none";
 }
 
 document.getElementById("b6").onclick = () =>  {
     document.getElementById('e6').style.display = "block";
-    document.getElementById('e5').style.display = "none";
-    document.getElementById('e7').style.display = "none";
 }
 
 document.getElementById("b7").onclick = () =>  {
     document.getElementById('e7').style.display = "block";
-    document.getElementById('e5').style.display = "none";
-    document.getElementById('e6').style.display = "none";
 }
 
