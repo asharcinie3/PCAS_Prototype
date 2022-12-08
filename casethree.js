4a74ff5848d949fede1fab160de1415ca707b304
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-08 04:54:13 -0500

    add rest of the videos

--- casethree.js
+++ casethree.js
@@ -15,3 +15,19 @@ document.getElementById("b10").onclick = () =>  {
     document.getElementById('e8').style.display = "none";
     document.getElementById('e9').style.display = "none";
 }
+
+var vid1 = document.getElementById("vid8");
+var vid2 = document.getElementById("vid9");
+var vid3 = document.getElementById("vid10");
+
+document.getElementById("r8").onclick = () =>  {
+    vid8.play();
+}
+
+document.getElementById("r9").onclick = () =>  {
+    vid9.play();
+}
+
+document.getElementById("r10").onclick = () =>  {
+    vid10.play();
+}
--- casetwo.js
+++ casetwo.js
@@ -15,3 +15,19 @@ document.getElementById("b7").onclick = () =>  {
     document.getElementById('e5').style.display = "none";
     document.getElementById('e6').style.display = "none";
 }
+
+var vid1 = document.getElementById("vid5");
+var vid2 = document.getElementById("vid6");
+var vid3 = document.getElementById("vid7");
+
+document.getElementById("r5").onclick = () =>  {
+    vid5.play();
+}
+
+document.getElementById("r6").onclick = () =>  {
+    vid6.play();
+}
+
+document.getElementById("r7").onclick = () =>  {
+    vid7.play();
+}
