f36da11002e7752fcfc5f6447b806fd5646de5f7
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-08 04:25:33 -0500

    fix play/pause

--- caseone.js
+++ caseone.js
@@ -26,3 +26,17 @@ document.getElementById("b4").onclick = () =>  {
     document.getElementById('e3').style.display = "none";
 }
 
+var video = document.getElementById('videoContent');
+videoAutoPlay = video.play();
+
+document.getElementById("playVideo").addEventListener("click", function(){     
+   this.classList.toggle('is-playing');     
+   if(this.classList.contains('is-playing')) {
+       this.innerHTML = "Play"
+       video.pause();
+   } else {
+       this.innerHTML = "Pause";
+       video.play();
+   }
+});
+
