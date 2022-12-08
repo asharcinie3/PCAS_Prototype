7648b2a04ae2f897a1cd5abc4a49224648bf7adb
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-08 04:36:30 -0500

    test

--- caseone.js
+++ caseone.js
@@ -1,3 +1,9 @@
+var vid1 = document.getElementById("vid1");
+var vid2 = document.getElementById("vid2");
+var vid3 = document.getElementById("vid3");
+var vid4 = document.getElementById("vid4");
+
+
 document.getElementById("b1").onclick = () =>  {
     document.getElementById('e1').style.display = "block";
     document.getElementById('e2').style.display = "none";
@@ -5,8 +11,13 @@ document.getElementById("b1").onclick = () =>  {
     document.getElementById('e4').style.display = "none";
 }
 
+document.getElementById("p1").onclick = () =>  {
+    vid1.play();
+}
+
 document.getElementById("b2").onclick = () =>  {
     document.getElementById('e2').style.display = "block";
+    vid2.play();
     document.getElementById('e1').style.display = "none";
     document.getElementById('e3').style.display = "none";
     document.getElementById('e4').style.display = "none";
@@ -14,6 +25,7 @@ document.getElementById("b2").onclick = () =>  {
 
 document.getElementById("b3").onclick = () =>  {
     document.getElementById('e3').style.display = "block";
+    vid3.play();
     document.getElementById('e1').style.display = "none";
     document.getElementById('e2').style.display = "none";
     document.getElementById('e4').style.display = "none";
@@ -21,10 +33,17 @@ document.getElementById("b3").onclick = () =>  {
 
 document.getElementById("b4").onclick = () =>  {
     document.getElementById('e4').style.display = "block";
+    vid4.play();
     document.getElementById('e1').style.display = "none";
     document.getElementById('e2').style.display = "none";
     document.getElementById('e3').style.display = "none";
 }
 
+function playVid() {
+    vid.play();
+}
 
+function pauseVid() {
+    vid.pause();
+}
 
