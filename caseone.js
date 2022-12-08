7208a6a951b0b3b7d8c42b6f4bf6147198df5d33
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-08 05:01:20 -0500

    fix autoplay feature

--- caseone.js
+++ caseone.js
@@ -1,42 +1,35 @@
-var vid1 = document.getElementById("vid1");
-var vid2 = document.getElementById("vid2");
-var vid3 = document.getElementById("vid3");
-var vid4 = document.getElementById("vid4");
-
 document.getElementById("b1").onclick = () =>  {
     document.getElementById('e1').style.display = "block";
     document.getElementById('e2').style.display = "none";
     document.getElementById('e3').style.display = "none";
     document.getElementById('e4').style.display = "none";
-    vid1.play();
 }
 
 document.getElementById("b2").onclick = () =>  {
     document.getElementById('e2').style.display = "block";
-    vid2.play();
     document.getElementById('e1').style.display = "none";
     document.getElementById('e3').style.display = "none";
     document.getElementById('e4').style.display = "none";
-    vid2.play();
 }
 
 document.getElementById("b3").onclick = () =>  {
     document.getElementById('e3').style.display = "block";
-    vid3.play();
     document.getElementById('e1').style.display = "none";
     document.getElementById('e2').style.display = "none";
     document.getElementById('e4').style.display = "none";
-    vid3.play();
 }
 
 document.getElementById("b4").onclick = () =>  {
     document.getElementById('e4').style.display = "block";
-    vid4.play();
     document.getElementById('e1').style.display = "none";
     document.getElementById('e2').style.display = "none";
     document.getElementById('e3').style.display = "none";
-    vid4.play();
 }
+
+var vid1 = document.getElementById("vid1");
+var vid2 = document.getElementById("vid2");
+var vid3 = document.getElementById("vid3");
+var vid4 = document.getElementById("vid4");
 
 document.getElementById("r1").onclick = () =>  {
     vid1.play();
