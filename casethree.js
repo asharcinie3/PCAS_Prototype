1ec9b58c546b8d5af359698a67edcbbb0e0de560
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-12 08:15:14 -0500

    use vars in case three

--- casethree.js
+++ casethree.js
@@ -2,44 +2,56 @@ var vid1 = document.getElementById("vid8");
 var vid2 = document.getElementById("vid9");
 var vid3 = document.getElementById("vid10");
 
-document.getElementById("b8").onclick = () =>  {
+var scenariobtn8 = document.getElementById("b8");
+var scenariobtn9 = document.getElementById("b9");
+var scenariobtn10 = document.getElementById("b10");
+
+var playbtn8 = document.getElementById("play8");
+var playbtn9 = document.getElementById("play9");
+var playbtn10 = document.getElementById("play10");
+
+var pausebtn8 = document.getElementById("pause8");
+var pausebtn9 = document.getElementById("pause9");
+var pausebtn10 = document.getElementById("pause10");
+
+scenariobtn8.onclick = () =>  {
     document.getElementById('e8').style.display = "block";
     document.getElementById('e9').style.display = "none";
     document.getElementById('e10').style.display = "none";
 }
 
-document.getElementById("b9").onclick = () =>  {
+scenariobtn9.onclick = () =>  {
     document.getElementById('e9').style.display = "block";
     document.getElementById('e8').style.display = "none";
     document.getElementById('e10').style.display = "none";
 }
 
-document.getElementById("b10").onclick = () =>  {
+scenariobtn10.onclick = () =>  {
     document.getElementById('e10').style.display = "block";
     document.getElementById('e8').style.display = "none";
     document.getElementById('e9').style.display = "none";
 }
 
-document.getElementById("play8").onclick = () =>  {
+playbtn8.onclick = () =>  {
     vid8.play();
 }
 
-document.getElementById("play9").onclick = () =>  {
+playbtn9.onclick = () =>  {
     vid9.play();
 }
 
-document.getElementById("play10").onclick = () =>  {
+playbtn10.onclick = () =>  {
     vid10.play();
 }
 
-document.getElementById("pause8").onclick = () =>  {
+pausebtn8.onclick = () =>  {
     vid8.pause();
 }
 
-document.getElementById("pause9").onclick = () =>  {
+pausebtn9.onclick = () =>  {
     vid9.pause();
 }
 
-document.getElementById("pause10").onclick = () =>  {
+pausebtn10.onclick = () =>  {
     vid10.pause();
 }
