c6fb6e78a9f9b95f74108bfa84b034f2cdd06bf3
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-12 06:55:53 -0500

    bugs

--- caseone.js
+++ caseone.js
@@ -131,7 +131,7 @@ vid1.onended = () => {
     document.getElementById("play1").disabled = true; 
     document.getElementById("play1").style.background = '#ccc';
 }
-if (vid1.currentTime == 0) {
+vid1.onplay = () => {
     document.getElementById("play1").disabled = false; 
     document.getElementById("play1").style.background = '#fff';
 }
