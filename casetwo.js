b6b0687eccff17b8b5a4abace9325354a54da622
asharcinie3 <ashley.arciniegga@gmail.com>
2022-12-12 07:43:11 -0500

    again

--- casetwo.js
+++ casetwo.js
@@ -80,7 +80,7 @@ pausebtn7.onclick = () =>  {
 /* add functionality for override */
 
 vid6.addEventListener('timeupdate', function(){
-    if(vid6.currentTime >= 2.5 && vid6.currentTime <= 2.9) {
+    if(vid6.currentTime >= 2.4 && vid6.currentTime <= 2.9) {
         document.getElementById("o6").disabled = false;
         document.getElementById("o6").style.background='#FF0000';
     }
